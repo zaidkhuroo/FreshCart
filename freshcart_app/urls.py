7689faf0283f15a473b53c305b528ae2d5377ca9
zaidkhuroo <khuroozaid@gmail.com>
2024-08-26 21:13:26 +0530

    webpages linked

--- freshcart_app/urls.py
+++ freshcart_app/urls.py
@@ -3,4 +3,13 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'), 
+    path('about', views.about, name='about'), 
+    path('checkout', views.checkout, name='checkout'), 
+    path('contact', views.contact, name='contact'), 
+    path('gallery', views.gallery, name='gallery'), 
+    path('account', views.account, name='account'), 
+    path('shop_detail', views.shop_detail, name='shop_detail'), 
+    path('shop', views.shop, name='shop'), 
+    path('wish', views.wish, name='wish'), 
+    path('cart', views.cart, name='cart'), 
 ]
--- freshcart_app/views.py
+++ freshcart_app/views.py
@@ -3,3 +3,21 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def home(request): 
     return render(request,'index.html')
+def about(request): 
+    return render(request,'about.html')
+def checkout(request): 
+    return render(request,'checkout.html')
+def contact(request): 
+    return render(request,'contact-us.html')
+def gallery(request): 
+    return render(request,'gallery.html')
+def account(request): 
+    return render(request,'my-account.html')
+def shop_detail(request): 
+    return render(request,'shop-detail.html')
+def shop(request): 
+    return render(request,'shop.html')
+def wish(request): 
+    return render(request,'wishlist.html')
+def cart(request): 
+    return render(request,'cart.html')
