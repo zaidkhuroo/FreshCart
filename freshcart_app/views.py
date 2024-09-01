26de0b39383c46c9a43ad54859adb0d42a5f60fe
zaidkhuroo <khuroozaid@gmail.com>
2024-09-01 23:59:43 +0530

    Products displayed in centralized data source approach

--- freshcart_app/views.py
+++ freshcart_app/views.py
@@ -1,5 +1,44 @@
 from django.shortcuts import render, HttpResponse, get_object_or_404
+from django.templatetags.static import static
 from . import models
+
+
+# Centralized product data
+products_data = {
+    'Carrot': {
+        'img': 'images/img-pro-01.jpg',
+        'name': 'Carrot',
+        'price': 40,
+        'origprice': 50,
+        'new': False,
+        'sale': True,
+        'description': 'Fresh Carrots are nutrient-dense...',
+    },
+    'Tomato': {
+        'img': 'images/img-pro-02.jpg',
+        'name': 'Tomato',
+        'price': 30,
+        'origprice': 60,
+        'new': False,
+        'sale': False,
+        'description': 'Fresh Tomatoes are juicy and rich...',
+    }
+}
+    
+def vegetables(request): 
+    products = list(products_data.values())  # Extracting all products for display
+    return render(request, 'vegetables.html', {'products': products})
+
+def product(request, product_name):
+    products = list(products_data.values()) 
+    product = products_data.get(product_name)
+    
+    if not product:
+        return HttpResponse("Product not found.", status=404)
+    product['img'] = static(product['img'])
+    
+    return render(request, 'product.html', {'product': product})
+
 
 # Create your views here.
 def home(request): 
@@ -20,26 +59,6 @@ def home(request):
     return render(request,'index.html', {'products':products})
 
 
-def vegetables(request): 
-    product1=models.Vegetables()
-    product1.img='static/images/img-pro-01.jpg'
-    product1.name='Carrot'
-    product1.price= '40'
-    product1.origprice= '50'
-    product1.new=False
-    product1.sale=True
-    
-    product2=models.Vegetables()
-    product2.img='static/images/img-pro-02.jpg'
-    product2.name='Tomato'
-    product2.price= '30'
-    product2.origprice= '60'
-    product2.new=False
-    product2.sale=False
-    
-    products=[product1,product2]
-    return render(request,'vegetables.html', {'products':products})
-
 def fruits(request): 
     return render(request,'fruits.html')
 def dairy(request): 
@@ -56,44 +75,8 @@ def wish(request):
     return render(request,'wishlist.html')
 def cart(request): 
     return render(request,'cart.html')
-def product(request): 
-    return render(request,'product.html')
+
 def login(request): 
     return render(request,'login.html')
 
 
-def product(request, product_name):
-    # Dummy data to match product_name
-    products_data = {
-        'Carrot': {
-            'img': 'static/images/img-pro-01.jpg',
-            'name': 'Carrot',
-            'price': 40,
-            'origprice': 50,
-            'new': False,
-            'sale': True,
-            'description': 'Fresh Carrots are nutrient-dense...',
-        },
-        'Tomato': {
-            'img': 'static/images/img-pro-02.jpg',
-            'name': 'Tomato',
-            'price': 30,
-            'origprice': 60,
-            'new': False,
-            'sale': False,
-            'description': 'Fresh Tomatoes are juicy and rich...',
-        }
-    }
-    
-    product = products_data.get(product_name)
-    
-    if not product:
-        return HttpResponse("Product not found.", status=404)
-    
-    return render(request, 'product.html', {'product': product})
-
-def fruits(request):
-    return render(request, 'fruits.html')
-
-def dairy(request):
-    return render(request, 'dairy.html')
