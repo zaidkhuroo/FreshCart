242ab2787eb60e2c7f9bce750ae7eb36a7bd9aee
zaidkhuroo <khuroozaid@gmail.com>
2024-09-11 00:06:04 +0530

    Added Gmail's SMTP to send emails through the contact form

--- main/settings.py
+++ main/settings.py
@@ -131,9 +131,3 @@ STATIC_ROOT=os.path.join(BASE_DIR,'assets')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'khuroozaid@gmail.com'
-EMAIL_HOST_PASSWORD = 'dmac ahmj alnz rexk'
