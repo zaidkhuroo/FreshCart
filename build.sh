480469ae04051b24f514b6dd486d6728b3e2188c
zaidkhuroo <khuroozaid@gmail.com>
2024-10-10 12:08:49 +0530

    Checkout Page made and several Bugs fixed

--- build.sh
+++ build.sh
@@ -9,10 +9,7 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 # Apply database migrations
+python manage.py makemigrations
 python manage.py migrate
 
-# Check if CREATE_SUPERUSER is set to true in the environment
-if [[ $CREATE_SUPERUSER == "true" ]]; then
-    # Create the superuser using environment variables for email and username
-    python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL" --username "$DJANGO_SUPERUSER_USERNAME"
-fi
+
